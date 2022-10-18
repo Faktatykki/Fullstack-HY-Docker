47fdfee1f826b2c861d47c5840ba1c4569f77df9
Sampo Kääriäinen <sampo.kaariainen@gmail.com>
2022-10-18 14:30:42 +0300

    12.19

--- todo-app/todo-frontend/src/Todos/TodoView.js
+++ todo-app/todo-frontend/src/Todos/TodoView.js
@@ -28,6 +28,7 @@ const TodoView = () => {
 
   const completeTodo = async (todo) => {
     await axios.put(`/todos/${todo._id}`, {
+      _id: todo._id,
       text: todo.text,
       done: true
     })
--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: "http://localhost:8080/api/",
 })
+
 
 export default apiClient
